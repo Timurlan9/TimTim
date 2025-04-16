7f2fbcb02533bb9ded7aada3c69c395c5fddb0d3
Timurlan9 <smex99991@gmail.com>
2025-04-16 12:40:04 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,30 +1,8 @@
-name: Deploy RL-Swarm Modal Login
+# Переходим в директорию проекта
+cd /root/rl-swarm/modal-login || { echo "Ошибка: директория не найдена"; exit 1; }
 
-on:
-  push:
-    branches: [ main ]  # Запускать при пуше в main
-  workflow_dispatch:    # Разрешить ручной запуск
-
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
-    
-    steps:
-    - name: Checkout code
-      uses: actions/checkout@v4
-
-    - name: Setup Node.js
-      uses: actions/setup-node@v4
-      with:
-        node-version: '20'  # Или ваша версия Node.js
-
-    - name: Install Yarn
-      run: npm install -g yarn
-
-    - name: Run deployment commands
-      run: |
-        cd /root/rl-swarm/modal-login
-        yarn up
-        yarn install
-        yarn build
-        yarn start
+# Устанавливаем зависимости и запускаем
+yarn up
+yarn install
+yarn build
+yarn start
